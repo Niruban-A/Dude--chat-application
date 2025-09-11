6249323e48daf9fcc1e5165da00a3e02f950e751
Niruban-A <nirubanchipriyan1453@gmail.com>
2025-09-11 22:02:23 +0530

    feat: adding home page

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Chatapp());
+}
+
+class Chatapp extends StatefulWidget {
+  const Chatapp({super.key});
+
+  @override
+  State<Chatapp> createState() => _ChatappState();
+}
+
+class _ChatappState extends State<Chatapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.dark(), home: HomePage());
+  }
+}
---
+++ lib/tools.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightGreen = Color(0xFFA8FBD3);   // #A8FBD3 primary
+  static const Color teal = Color(0xFF4FB7B3);         // #4FB7B3
+  static const Color blue = Color(0xFF637AB9);         // #637AB9
+  static const Color darkBlue = Color(0xFF31326F);     // #31326F
+}
