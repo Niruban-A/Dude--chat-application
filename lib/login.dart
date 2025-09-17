57877ba2aa182d374ca7e24559da840094054968
Niruban-A <nirubanchipriyan1453@gmail.com>
2025-09-17 20:47:49 +0530

    feat:google signin

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,9 @@
 import 'package:chat_application/tools.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
@@ -9,7 +12,53 @@ class Loginpage extends StatefulWidget {
   State<Loginpage> createState() => _LoginpageState();
 }
 
+
 class _LoginpageState extends State<Loginpage> {
+
+User? user;
+final GoogleSignIn googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+  @override
+  void initState(){
+    super.initState();
+    
+
+
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      // Trigger the Google Sign-In flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      
+      if (googleUser == null) {
+        // User canceled the sign-in
+        return;
+      }
+
+      // Obtain the auth details
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      // Create a credential for Firebase
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+      
+      ); // Sign in to Firebase
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      setState(() {
+        user = userCredential.user;
+      });
+    } catch (e) {
+      print("Sign in failed: $e");
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
