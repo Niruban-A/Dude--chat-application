493a0019d3962dce6b5ade9b4277d47e2ff59699
Niruban-A <nirubanchipriyan1453@gmail.com>
2025-09-24 20:54:51 +0530

    feat: improving the authetication flow

--- lib/login.dart
+++ lib/login.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:async/async.dart' show unawaited;
 
 class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
@@ -14,9 +15,24 @@ class Loginpage extends StatefulWidget {
 
 class _LoginpageState extends State<Loginpage> {
   User? user;
-  final GoogleSignIn googleSignIn = GoogleSignIn(
-    scopes: ['email', 'https://www.googleapis.com/auth/contacts.readonly'],
-  );
+  final String clientid="596493783703-5blvnt3egqq5kcrio3ctrbapnqbgnjgs.apps.googleusercontent.com";
+  final GoogleSignIn googleSignIn = GoogleSignIn.instance;
+  final GoogleSignIn signIn = GoogleSignIn.instance;
+  
+  // unawaited(
+  // signIn.initialize(clientId: clientId, serverClientId: serverClientId).then((
+  // _,
+  // ) {
+  // signIn.authenticationEvents
+  // .listen(_handleAuthenticationEvent)
+  // .onError(_handleAuthenticationError);
+
+  // /// This example always uses the stream-based approach to determining
+  // /// which UI state to show, rather than using the future returned here,
+  // /// if any, to conditionally skip directly to the signed-in state.
+  // signIn.attemptLightweightAuthentication();
+  // }),
+  // );
   @override
   void initState() {
     super.initState();
@@ -25,7 +41,7 @@ class _LoginpageState extends State<Loginpage> {
   Future<void> signInWithGoogle() async {
     try {
       // Trigger the Google Sign-In flow
-      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+      final GoogleSignInAccount? googleUser = await googleSignIn.authenticate();
 
       if (googleUser == null) {
         // User canceled the sign-in
@@ -68,7 +84,9 @@ class _LoginpageState extends State<Loginpage> {
             ),
             SizedBox(height: 30),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                await signInWithGoogle();
+              },
               style: OutlinedButton.styleFrom(foregroundColor: Colors.red),
               child: Text(
                 "Sign in with google",
